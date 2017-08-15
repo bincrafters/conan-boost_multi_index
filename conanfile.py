e58125c3663ccc08cbfec8d7107bd2f29fab7282
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:16 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostMulti_IndexConan(ConanFile):
     name = "Boost.Multi_Index"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/multi_index"
+    url = "https://github.com/bincrafters/conan-boost-multi_index"
+    source_url = "https://github.com/boostorg/multi_index"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "multi_index"
@@ -32,7 +32,7 @@ class BoostMulti_IndexConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
