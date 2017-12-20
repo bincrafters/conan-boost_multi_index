d833ba6173f8ce8519644d7b15347d284dc14b53
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 12:47:32 -0600

    Add build_policy_missing to make levelgroup members build on-demand.

--- conanfile.py
+++ conanfile.py
@@ -66,5 +66,8 @@ class BoostMulti_IndexConan(ConanFile):
         result = super(self.__class__, self).env
         result['PYTHONPATH'] = [os.path.dirname(__file__)] + result.get('PYTHONPATH',[])
         return result
+    @property
+    def build_policy_missing(self):
+        return (getattr(self, 'is_in_cycle_group', False) and not getattr(self, 'is_header_only', True)) or super(self.__class__, self).build_policy_missing
 
     # END
