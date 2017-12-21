851d49ddd3446d7139494387248214a90d19a272
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:33 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,28 +3,28 @@ from conans import ConanFile
 
 class BoostMulti_IndexConan(ConanFile):
     name = "Boost.Multi_Index"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Bind/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Detail/1.65.1@bincrafters/testing", \
-        "Boost.Foreach/1.65.1@bincrafters/testing", \
-        "Boost.Functional/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Iterator/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Serialization/1.65.1@bincrafters/testing", \
-        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Tuple/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Bind/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Detail/1.66.0@bincrafters/testing", \
+        "Boost.Foreach/1.66.0@bincrafters/testing", \
+        "Boost.Functional/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Iterator/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Serialization/1.66.0@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Tuple/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing"
 
     lib_short_names = ["multi_index"]
     is_header_only = True
@@ -32,9 +32,9 @@ class BoostMulti_IndexConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-multi_index"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
