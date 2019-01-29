d9db5c808633d4ef7c4de596e836778784081bcc
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:25 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostMulti_IndexConan(base.BoostBaseConan):
     name = "boost_multi_index"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_multi_index"
     lib_short_names = ["multi_index"]
     header_only_libs = ["multi_index"]
